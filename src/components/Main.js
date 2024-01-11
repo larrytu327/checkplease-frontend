e45a7bcc7e488124371a0758c6b02f947e824c20
larrytu327 <larrytu327@gmail.com>
2024-01-10 21:28:58 -0800

    setup Main and Header

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import People from "../pages/People"
+import Show from "../pages/Show"
+const Main = () => {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<People />} />
+        <Route path="/people/:id" element={<Show />} />
+      </Routes>
+    </main>
+  )
+}
+
+export default Main
